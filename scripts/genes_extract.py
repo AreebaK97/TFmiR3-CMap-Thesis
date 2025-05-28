0aa7eee3ba1ae94a5959b9d1eb45c897f0dcd840
Areeba Khan <93472428+AreebaK97@users.noreply.github.com>
2025-05-28 12:55:12 +0200

    Update genes_extract.py

--- scripts/genes_extract.py
+++ scripts/genes_extract.py
@@ -23,14 +23,23 @@ def extract_genes(method, num_genes, node_properties_file=None, network_properti
         if not degs_file:
             raise ValueError("DEGs file must be provided when method is 'degs'.")
 
+        pval_col = "padj" if "padj" in cancer_df.columns else "FDR"
+        logfc_col = "log2FoldChange" if "log2FoldChange" in cancer_df.columns else "logFC"
+        pval_raw_col = "pvalue" if "pvalue" in cancer_df.columns else "PValue"
+        cancer_df = cancer_df[(cancer_df[pval_col] < 0.01) & (abs(cancer_df[logfc_col]) > log2fc_threshold)].sort_values(pval_raw_col)
+        if "gene_name" in cancer_df.columns:
+            genes = cancer_df["gene_name"].astype(str)
+        else:
+            genes = cancer_df.index.astype(str)
+
+        genes = genes.str.replace(r"\.\d+$", "", regex=True)
+        cancer_df = cancer_df.assign(cleaned_gene=genes)
+        upregulated = cancer_df[cancer_df[logfc_col] > log2fc_threshold].head(num_genes)["cleaned_gene"].tolist()
+        downregulated = cancer_df[cancer_df[logfc_col] < -log2fc_threshold].head(num_genes)["cleaned_gene"].tolist()
         
-        cancer_df = cancer_df[(cancer_df["padj"] < 0.01) & (abs(cancer_df["log2FoldChange"]) > log2fc_threshold)].sort_values("pvalue")
-
-        # upregulated = cancer_df[cancer_df["log2FoldChange"] > log2fc_threshold].nlargest(num_genes, "pvalue")["gene_name"].tolist()
-        # downregulated = cancer_df[cancer_df["log2FoldChange"] < -1].nsmallest(num_genes, "pvalue")["gene_name"].tolist()
-        upregulated = cancer_df[cancer_df["log2FoldChange"] > log2fc_threshold].head(num_genes)["gene_name"].tolist()
-
-        downregulated = cancer_df[cancer_df["log2FoldChange"] < -log2fc_threshold].head(num_genes)["gene_name"].tolist()
+        # cancer_df = cancer_df[(cancer_df["padj"] < 0.01) & (abs(cancer_df["log2FoldChange"]) > log2fc_threshold)].sort_values("pvalue")
+        #upregulated = cancer_df[cancer_df["log2FoldChange"] > log2fc_threshold].head(num_genes)["gene_name"].tolist()
+        #downregulated = cancer_df[cancer_df["log2FoldChange"] < -log2fc_threshold].head(num_genes)["gene_name"].tolist()
 
     else:
         if not node_properties_file or not network_properties_file:
@@ -45,18 +54,18 @@ def extract_genes(method, num_genes, node_properties_file=None, network_properti
 
         if method == "hub":
             genes = filtered_data.sort_values(by="degree", ascending=False)["node.ID"].tolist()
-            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values("pvalue")
+            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values(pval_raw_col)
             upregulated = method_genes[method_genes["log2FoldChange"] > log2fc_threshold].head(num_genes)["gene_name"].tolist()
             downregulated = method_genes[method_genes["log2FoldChange"] < -log2fc_threshold].head(num_genes)["gene_name"].tolist()
         elif method == "mds":
             genes = filtered_data[filtered_data["MDS"] == "True"]["node.ID"].tolist()
-            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values("pvalue")
+            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values(pval_raw_col)
             upregulated = method_genes[method_genes["log2FoldChange"] > log2fc_threshold].head(num_genes)["gene_name"].tolist()
             downregulated = method_genes[method_genes["log2FoldChange"] < -log2fc_threshold].head(num_genes)["gene_name"].tolist()
         elif method == "vertex":
             vertex_sort = network_data["hotspots"]["vertex.sort"]
             genes = list(vertex_sort["VS.core"] + vertex_sort["VS.top"])
-            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values("pvalue")
+            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values(pval_raw_col)
             upregulated = method_genes[method_genes["log2FoldChange"] > log2fc_threshold].head(num_genes)["gene_name"].tolist()
             downregulated = method_genes[method_genes["log2FoldChange"] < -log2fc_threshold].head(num_genes)["gene_name"].tolist()
         elif method == "bfs":
@@ -66,7 +75,7 @@ def extract_genes(method, num_genes, node_properties_file=None, network_properti
             genes = []
             for level in last_three_levels:
                 genes.extend(bfs_dict[level])
-            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values("pvalue")
+            method_genes = cancer_df[cancer_df["gene_name"].isin(genes)].sort_values(pval_raw_col)
             upregulated = method_genes[method_genes["log2FoldChange"] > log2fc_threshold].head(num_genes)["gene_name"].tolist()
             downregulated = method_genes[method_genes["log2FoldChange"] < -log2fc_threshold].head(num_genes)["gene_name"].tolist()
         else:
